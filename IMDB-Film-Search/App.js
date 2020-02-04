e3f6a6b84302ebcc80a763996403baeeed7b3338
Andrew Weiss <wandrew8@gmail.com>
2020-02-04 15:50:45 -0800

    initial files added with dependencies installed

---
+++ IMDB-Film-Search/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>IMDB Film Search</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#444',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 34,
+    color: '#fff',
+    
+  },
+});
